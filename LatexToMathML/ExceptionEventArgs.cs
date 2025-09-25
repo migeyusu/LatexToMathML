658f93f34b1719a473aeebc18d40bd6c7937863b
jie.zhu <981975491@qq.com>
2025-09-25 17:47:23 +0800

    initial add

---
+++ LatexToMathML/ExceptionEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LatexToMathML
+{
+    public class ExceptionEventArgs : EventArgs
+    {
+        public ExceptionEventArgs(String message)
+        {
+            this.Message = message;
+        }
+
+        public String Message { get; private set; }
+    }
+}
